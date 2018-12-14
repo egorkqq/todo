c4382c5783b0047e34a7016cd11891032dfb0aa1
Egor Sidorov <egorkqq@gmail.com>
2018-12-14 20:00:31 +0500

    feat:  add opportunity to check/uncheck tasks

--- src/components/TodoListItem/index.js
+++ src/components/TodoListItem/index.js
@@ -2,17 +2,34 @@ import React, { Component } from "react";
 import "./TodoListItem.sass";
 
 export default class TodoListItem extends Component {
-  state = {};
+  state = {
+    done: false
+  };
+
+  handleClick = () => {
+    this.setState({
+      done: this.state.done ? false : true
+    });
+  };
 
   render() {
     const { important, label } = this.props;
+    const { done } = this.state;
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
     const style = {
       color: important ? "tomato" : "grey",
       fontWeight: important ? 800 : 400
     };
     return (
-      <span className="todo-list-item">
-        <span className="todo-list-item-label" style={style}>
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this.handleClick}
+        >
           {label}
         </span>
         <span>

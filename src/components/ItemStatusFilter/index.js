6bdbef295dcd3fb02c543ff978a62f5f6b97415d
Egor Sidorov <egorkqq@gmail.com>
2018-12-14 19:02:54 +0500

    refactor: functions replaced with clasess in componets that need state

--- src/components/ItemStatusFilter/index.js
+++ src/components/ItemStatusFilter/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-
-const ItemStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button className="btn btn-info">All</button>
-      <button className="btn btn-outline-secondary">Active</button>
-      <button className="btn btn-outline-secondary">Done</button>
-    </div>
-  );
-};
+import React, { Component } from "react";
+class ItemStatusFilter extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="btn-group">
+        <button className="btn btn-info">All</button>
+        <button className="btn btn-outline-secondary">Active</button>
+        <button className="btn btn-outline-secondary">Done</button>
+      </div>
+    );
+  }
+}
 
 export default ItemStatusFilter;

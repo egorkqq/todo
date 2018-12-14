ffc174c56dca0a8b64285efd858eab739e214cb8
egorkqq <egorkqq@gmail.com>
2018-12-14 23:36:31 +0500

    feat: opportunity to add and delete items

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./App.sass";
 
@@ -6,25 +6,51 @@ import SearchPanel from "./../SearchPanel/";
 import AppHeader from "./../AppHeader/";
 import TodoList from "./../TodoList/";
 import ItemStatusFilter from "./../ItemStatusFilter/";
+import AddItem from "./../AddItem/";
 
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Fuck Yourself", important: true, id: 2 },
-    { label: "Yankees go home", important: false, id: 3 }
-  ];
+export default class App extends Component {
+  maxId = 100;
+  state = {
+    todoData: [
+      { label: "Drink Coffee", important: false, id: 1 },
+      { label: "Beat Yourself", important: true, id: 2 },
+      { label: "Yankees go home", important: false, id: 3 }
+    ]
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  deleteItem = id => {
+    this.setState(({ todoData }) => {
+      const index = todoData.findIndex(el => el.id === id);
+
+      return {
+        todoData: [...todoData.slice(0, index), ...todoData.slice(index + 1)]
+      };
+    });
+  };
+  addItemToList = text => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    };
+    this.setState(({ todoData }) => {
+      return {
+        todoData: [...todoData.slice(0), newItem]
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList onDeleted={this.deleteItem} todos={this.state.todoData} />
+        <AddItem onAddItem={this.addItemToList} className="top-panel d-flex" />
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}

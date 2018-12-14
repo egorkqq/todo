a0ae5e25b49bc73e1c6ddfc930784b77c52a83f4
Egor Sidorov <egorkqq@gmail.com>
2018-12-14 12:20:42 +0500

    fix: react.fragment to  just fragment

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
 const el = (
-  <React.Fragment>
+  <Fragment>
     <h1 children="My Todo List" />
     <input type="text" placeholder="search" />
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
     </ul>
-  </React.Fragment>
+  </Fragment>
 );
 ReactDOM.render(el, document.getElementById("root"));

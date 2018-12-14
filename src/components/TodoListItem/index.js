a84e4e6021a99a4cec57252ea4a295ba3c002373
Egor Sidorov <egorkqq@gmail.com>
2018-12-14 18:52:13 +0500

    fix: fix todoListItem styles

--- src/components/TodoListItem/index.js
+++ src/components/TodoListItem/index.js
@@ -11,12 +11,14 @@ const TodoListItem = ({ label, important = false }) => {
       <span className="todo-list-item-label" style={style}>
         {label}
       </span>
-      <button className="btn btn-outline-success btn-sm">
-        <i className="fa fa-exclamation" />
-      </button>
-      <button className="btn btn-outline-danger btn-sm">
-        <i className="fa fa-trash-o" />
-      </button>
+      <span>
+        <button className="btn btn-outline-success btn-sm">
+          <i className="fa fa-exclamation" />
+        </button>
+        <button className="btn btn-outline-danger btn-sm">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
     </span>
   );
 };

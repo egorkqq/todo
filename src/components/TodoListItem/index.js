8003f77f04a548096702ee8b454cb5967b448b49
Egor Sidorov <egorkqq@gmail.com>
2018-12-14 18:46:57 +0500

    fix: change project structure to best practice

---
+++ src/components/TodoListItem/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./TodoListItem.sass";
+
+const TodoListItem = ({ label, important = false }) => {
+  const style = {
+    color: important ? "tomato" : "grey",
+    fontWeight: important ? 800 : 400
+  };
+  return (
+    <span className="todo-list-item">
+      <span className="todo-list-item-label" style={style}>
+        {label}
+      </span>
+      <button className="btn btn-outline-success btn-sm">
+        <i className="fa fa-exclamation" />
+      </button>
+      <button className="btn btn-outline-danger btn-sm">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
+};
+
+export default TodoListItem;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import React, { Fragment } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-const el = (
-  <Fragment>
-    <h1 children="My Todo List" />
-    <input type="text" placeholder="search" />
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
-  </Fragment>
-);
-ReactDOM.render(el, document.getElementById("root"));
+import App from "./components/App/";
+ReactDOM.render(<App />, document.getElementById("root"));

cb706082e7bf0e54db40806befba7bfa906553e0
egorkqq <egorkqq@gmail.com>
2019-01-14 19:37:23 +0500

    Updates

---
+++ precache-manifest.524f34faa1947f625cf1e3ca804f5603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "7ed38662e3667a256932",
+    "url": "/todo/static/js/main.7ed38662.chunk.js"
+  },
+  {
+    "revision": "c26583f2bc635d6b5c6e",
+    "url": "/todo/static/js/1.c26583f2.chunk.js"
+  },
+  {
+    "revision": "7ed38662e3667a256932",
+    "url": "/todo/static/css/main.86f8b59e.chunk.css"
+  },
+  {
+    "revision": "5801439b889cf959946c632ea9585f7a",
+    "url": "/todo/index.html"
+  }
+];
